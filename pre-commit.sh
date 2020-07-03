9bf877dc51decbdf92e250ec1452a4165a6f53f3
Sofia Swidarowicz <sofia.swidarowicz@gmail.com>
2020-07-03 08:56:53 +0200

    Script to run swift lint autocorrect
    Runs in on staged and unstaged files

---
+++ pre-commit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "post-commit started"
+# Run SwiftLint
+START_DATE=$(date +"%s")
+
+SWIFT_LINT=/usr/local/bin/swiftlint
+
+# Run SwiftLint for given filename
+exec_swiftlint() {
+    local filename="${1}"
+    print("File is: ${filename}")
+        if [[ "${filename##*.}" == "swift" ]]; then
+            if [[ "${filename}" != "AutoMockable"]]; then
+            ${SWIFT_LINT} autocorrect --path "${filename}"
+            ${SWIFT_LINT} lint --path "${filename}"
+            fi
+        fi
+}
+
+if [[ -e "${SWIFT_LINT}" ]]; then
+    echo "SwiftLint version: $(${SWIFT_LINT} version)"
+    # Run for both staged and unstaged files
+    git diff --name-only | while read filename; do exec_swiftlint "${filename}"; done
+    git diff --cached --name-only | while read filename; do run_swiftlint "${filename}"; done
+else
+    echo "${SWIFT_LINT} is not installed. Please install it first from https://www.github.com/realm/SwiftLint"
+    exit 0
+fi
+
+END_DATE=$(date +"%s")
+
+DIFF=$(($END_DATE - $START_DATE))
+echo "SwiftLint took $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds to complete."
+
+echo "post-commit finished"
